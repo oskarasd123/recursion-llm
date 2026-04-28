89e8db4b3d09512afa76f1d5a0e4603d2cc92a76
oskarasd123 <o@b.c>
2026-04-28 13:02:41 +0300

    inference output color based on thinking depth

--- inference.py
+++ inference.py
@@ -67,6 +67,8 @@ model.eval()
 model_opt = model
 #model_opt = torch.compile(model, dynamic=True)
 
+def color_bg(text, r, g, b):
+    return f"\x1b[48;2;{int(r)};{int(g)};{int(b)}m{text}\x1b[0m"
 
 try:
     while True:
@@ -82,24 +84,38 @@ try:
             prev_prompt = text
             ids = tokenizer(text, return_tensors="pt")["input_ids"].cuda().squeeze(0)
             new_ids = []
+            new_output_weight_depths = []
             print(text, end="")
         else:
             print("\033[F", end='') # move cursor to the start of the previous line
         for i in range(80):
-            logits = model_opt(ids.unsqueeze(0),
-                        loop_steps=loop_steps,
-            ).squeeze(0)
+            logits, output_weights = model_opt(ids.unsqueeze(0),
+                     loop_steps=loop_steps,
+                     return_output_weights=True,
+            )
+            output_weights : Tensor = output_weights.squeeze(0)
+            logits = logits.squeeze(0)
             new_id = torch.distributions.Categorical(logits=logits[-1]).sample()
             ids = torch.cat([ids, new_id.unsqueeze(0)], 0)
+            output_weight_depth = (torch.arange(loop_steps, device="cuda")[None,:] * output_weights).sum(-1)[-1].item()
             new_ids.append(new_id.item())
+            new_output_weight_depths.append(output_weight_depth)
             if new_id.item() == tokenizer.eos_token_id:
                 print("<eos_token>")
                 break
             try: 
                 new_text = tokenizer.decode(new_ids).encode().decode() # if the string doesn't contain errors
                 assert "�" not in new_text
-                print(new_text, end="", flush=True)
+                if loop_steps > 1:
+                    t = np.mean(new_output_weight_depths) / (loop_steps-1)
+                    r = 0 if len(new_ids) == 1 else 128
+                    g = t**0.5*255
+                    b = (1-t)**0.5*255
+                    print(color_bg(new_text, r, g, b), end="", flush=True)
+                else:
+                    print(new_text, end="", flush=True)
                 new_ids = []
+                new_output_weight_depths = []
             except Exception as e:
                 if len(new_ids) > 4:
                     # print one token
